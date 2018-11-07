e4bde50dd00795f6ef0c8eb45ffd0333d1425ab7
JeffRisberg <jrisberg@aol.com>
2018-11-06 18:01:04 -0800

    added json form example

---
+++ src/main/java/com/company/examples/PostJsonFormExample.java
@@ -0,0 +1,52 @@
+package com.company.examples;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.*;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+
+public class PostJsonFormExample {
+    public static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
+
+    public static final MediaType FORM = MediaType.parse("application/form-data");
+
+    OkHttpClient client = new OkHttpClient();
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    JSONParser jsonParser = new JSONParser();
+
+    String post(String url) throws IOException {
+
+        RequestBody formBody = new FormBody.Builder()
+                .add("search", "Jurassic Park")
+                .add("tenantId", "10000")
+                .add("type", "Salesforce")
+                .add("parameters", "{\"alpha\": 56, \"beta\": \"elephant\"}")
+                .add("data", "{\"gamma\": 123, \"delta\": \"zebra\"}")
+                .build();
+        Request request = new Request.Builder()
+                .post(formBody)
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            JSONObject responseBodyAsJson = (JSONObject) jsonParser.parse(response.body().string());
+
+            return responseBodyAsJson.toJSONString();
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        PostJsonFormExample example = new PostJsonFormExample();
+
+        String response = example.post("http://postman-echo.com/post");
+        System.out.println(response);
+    }
+}
